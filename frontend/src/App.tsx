5209b76a58153e0fd4d5212215580571e9760487
user <user@pop-os.home>
2025-07-09 15:46:47 +0200

    intialises the front with vite and react and dockerises it

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">Welcome to Your React + Tailwind App</h1>
+      <p className="text-lg text-gray-700">
+        Start building your frontend!
+      </p>
+    </div>
+  )
+}
+
+export default App
